a62360714c7d1cd79f0c5972c248a407284b9df5
wq70 <zrb9080@gmail.com>
2026-02-06 11:38:16 +0800

    Add files via upload

--- update-notification.js
+++ update-notification.js
@@ -2,7 +2,7 @@
 class UpdateNotification {
   constructor() {
     this.storageKey = 'update_notification_dismissed';
-    this.currentVersion = '0.0.6'; // 当前更新版本号
+    this.currentVersion = '0.0.7'; // 当前更新版本号
     this.countdownSeconds = 5;
     this.countdownInterval = null;
   }
